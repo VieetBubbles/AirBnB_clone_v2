f5132999d4ff7e1791029a5a4931409e233d5eb3
VieetBubbles <537@holbertonschool.com>
2019-08-20 01:32:20 +0000

    Fixed underscore typo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,17 +4,19 @@
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p /data/web-static/shared/
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+<head>
+</head>
+<body>
+<p>Holberton School</p>
+</body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sed -i "/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current;}" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
