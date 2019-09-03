165fedf3f82b5e030fbf490bdc0c12b250ddf7d1
VieetBubbles <537@holbertonschool.com>
2019-09-03 21:49:56 +0000

    Changed cities method to now use store

--- models/state.py
+++ models/state.py
@@ -4,7 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from os import getenv
-
+import models
+from models.city import City
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -17,15 +18,15 @@ class State(BaseModel, Base):
     name = Column(String(128),
                   nullable=False)
 
-    cities = relationship("City",
-                          backref="state",
-                          cascade="all, delete-orphan")
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete-orphan")
+    else:
         @property
-        def cities():
+        def cities(self):
             empty = []
-            for value in self.cities:
+            for n, value in models.storage.all(City).items():
                 if value.state_id == self.id:
                     empty.append(value)
             return(empty)
